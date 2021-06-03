961b83bc6023e2b4545ac1dc1a095b6239e8d116
Paul Brubaker <pbrubak@gmail.com>
2021-06-03 14:56:02 -0700

    Working add feature

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 // import functions
+import { add } from './operations.js';
 
 // reference needed DOM elements
 const inputAddX = document.getElementById('input-add-x');
@@ -6,12 +7,13 @@ const inputAddY = document.getElementById('input-add-y');
 const btnAdd = document.getElementById('btn-add');
 const resultAdd = document.getElementById('result-add');
 
-console.log(inputAddX, inputAddY, btnAdd, resultAdd);
-
 // set event listeners 
 btnAdd.addEventListener('click', () => {
-  console.log('add button clicked');
+  const x = Number(inputAddX.value);
+  const y = Number(inputAddY.value);
+// do any needed work with the value(s)
+  const sum = add(x, y);
+// update DOM to reflect new value(s)
+  resultAdd.textContent = sum;
 })
-  // get user input(s)
-  // do any needed work with the value(s)
-  // update DOM to reflect new value(s)
+  
